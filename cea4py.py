c1927ee6b615e0c2d0f0b98cd7945161b6b93e6a
rkanai <meck.rk@gmail.com>
2016-05-16 23:45:31 +0900

    fuelを手軽にいじれるようにした

--- cea4py.py
+++ cea4py.py
@@ -53,11 +53,14 @@ import csv
 
 # 環境によって都度変更のこと
 if 'Windows' == platform.system():
-    cmd = 'D:\\CEA2\\fCEA2.exe'
+    cmd = 'C:\\CEA\\fCEA2.exe'
 
 if 'Darwin' == platform.system():
     cmd = '~/Applications/NASAcea/FCEA2'
 
+# 燃料の選択
+fuel = 'C10H21,n-decyl'
+fuel_temperature = 300
 
 # CEA を python の関数として扱う．fac仮定
 def cea_fac (OF, Pc, AR, CR, pa = 0.101325):
@@ -71,12 +74,12 @@ def cea_fac (OF, Pc, AR, CR, pa = 0.101325):
       sup,ae/at= %.3f, ac/at = %.3f
 
     react  
-      fuel=C2H5OH(L) wt=100  t,k=288
+      fuel=%s　wt=100 t,k=%d
       oxid=O2(L) wt=100  t,k=90.17
     output  
         plot p ispfz ivacfz cffz
     end
-    """ % (OF, Pc*10, AR, CR)
+    """ % (OF, Pc*10, AR, CR, fuel, fuel_temperature)
 # fac オプションを付けると出力は inj, chamber, throat, exit の順になる．
 # exit の指定は出口圧力比(pi/p)，開口比(suparまたはsup,ae/at)などの方法があり，複数指定すると
 # exit の出力も複数になるが，ここではある決まった(作った)ノズルに対する性能を見たいので開口比で指定．
@@ -136,6 +139,9 @@ def cea_fac (OF, Pc, AR, CR, pa = 0.101325):
 if __name__ == '__main__': # テスト用．これ以降がなくても他ファイルからモジュールとして関数は呼べる
     # とりあえず Isp vs AR のグラフを出力してみます
     vsar_data_file = 'csvfac_ispvsar.csv'
+    vspc_data_file = 'csvfac_ispvspc.csv'
+    vsof_data_file = 'csvfac_ispvsof.csv'
+    vspa_data_file = 'csvfac_ispvspa.csv'
 
     if 'Windows' == platform.system():
         fp = FontProperties(fname=r'C:\WINDOWS\Fonts\ipaexg.ttf') # ipaゴシック好き フリーだしおすすめです
@@ -154,24 +160,24 @@ if __name__ == '__main__': # テスト用．これ以降がなくても他ファ
     plt.figure()
 
     # ---- パラメータ指定 ----
-    pc_array = np.linspace(1, 7, 4) # 適っ当にPc=1, 3, 5, 7MPa
-    ar_array = np.linspace(2, 10, 50) # ARは適っ当に2から10までを50分割してみる
-    vsar_data_array = np.zeros((len(ar_array), 6))
+    pc_array1 = np.linspace(0.5, 8.5, 5) # 適っ当にPc=1, 3, 5, 7MPa
+    ar_array1 = np.linspace(1.01, 10, 100) # ARは適っ当に2から10までを50分割してみる
+    vsar_data_array = np.zeros((len(ar_array1), 6))
     save_array = np.zeros([1,6]) # CSVに書き込むデータのストック場所
     # ---- コマンドをループさせる ----
     # この例では開口比ARを横軸、Ispを縦軸にとってPLOTする，Pcは複数条件振る
-    for Pc in pc_array:
+    for Pc in pc_array1:
         k = 0
-        for AR in ar_array:
+        for AR in ar_array1:
             data = cea_fac(OF = 2, Pc = Pc, AR = AR, CR = 4) # 昔の人が見たら発狂しそうな記法 CRとO/Fは適当
             print data
             vsar_data_array[k] = np.array(data)
             k+=1
         g = 9.80665
         Isp = vsar_data_array[:,2] / g # 海面高度比推力 sec
+        cf =  vsar_data_array[:,5]
         # ---- PLOT ----
-        plt.plot(ar_array, Isp, label='Pc=%.1f' % (Pc))
-        # ----      ----
+        plt.plot(ar_array1, cf, label='Pc=%.1f' % (Pc))
         save_array = np.vstack([save_array, vsar_data_array])
 
     # ---- CSVで結果を保存 ----
@@ -179,10 +185,118 @@ if __name__ == '__main__': # テスト用．これ以降がなくても他ファ
     np.savetxt(vsar_data_file, save_array, fmt='%.3f', delimiter=',', header = header)
 
     # ---- PLOTの設定 ----
-    plt.xlim(2, 10)
+    plt.xlim(1, 10)
     plt.xlabel('AR')
     plt.ylabel('Isp (sec)')
     plt.grid()
-    plt.title('LOX/C2H5OH 100% frozen nfz=3')
+    plt.title('LOX/%s 100% frozen nfz=3' % (fuel))
     plt.legend(loc='best', fontsize=12)
     plt.savefig('ceatest_IspvsAR.png')
+    plt.close()
+
+
+    # ---- パラメータ指定 ----
+    ar_array2 = np.linspace(2, 22, 5) # 適っ当にPc=1, 3, 5, 7MPa
+    pc_array2 = np.linspace(0.5, 10, 100) # ARは適っ当に2から10までを50分割してみる
+    vspc_data_array = np.zeros((len(pc_array2), 6))
+    save_array = np.zeros([1,6]) # CSVに書き込むデータのストック場所
+    # ---- コマンドをループさせる ----
+    # この例では開口比ARを横軸、Ispを縦軸にとってPLOTする，Pcは複数条件振る
+    for AR in ar_array2:
+        k = 0
+        for Pc in pc_array2:
+            data = cea_fac(OF = 2, Pc = Pc, AR = AR, CR = 4) # 昔の人が見たら発狂しそうな記法 CRとO/Fは適当
+            print data
+            vspc_data_array[k] = np.array(data)
+            k+=1
+        g = 9.80665
+        Isp = vspc_data_array[:,2] / g # 海面高度比推力 sec
+        # ---- PLOT ----
+        plt.plot(pc_array2, Isp, label='AR=%.1f' % (AR))
+        save_array = np.vstack([save_array, vspc_data_array])
+
+    # ---- CSVで結果を保存 ----
+    header = u'Pc[MPa], O/F[-], Isp_sea[sec], Isp_vac[sec], AR[-], cf[-]'
+    np.savetxt(vspc_data_file, save_array, fmt='%.3f', delimiter=',', header = header)
+
+    # ---- PLOTの設定 ----
+    plt.xlim(0.5, 10)
+    plt.xlabel('Pc')
+    plt.ylabel('Isp (sec)')
+    plt.grid()
+    plt.title('LOX/%s 100% frozen nfz=3' % (fuel))
+    plt.legend(loc='best', fontsize=12)
+    plt.savefig('ceatest_IspvsPc.png')
+    plt.close()
+
+    # ---- パラメータ指定 ----
+    ar_array3 = np.linspace(3, 10, 15) # 適っ当にPc=1, 3, 5, 7MPa
+    of_array3 = np.linspace(2.0, 2.5, 11) # ARは適っ当に2から10までを50分割してみる
+    vsof_data_array = np.zeros((len(of_array3), 6))
+    save_array = np.zeros([1,6]) # CSVに書き込むデータのストック場所
+    # ---- コマンドをループさせる ----
+    # この例では開口比ARを横軸、Ispを縦軸にとってPLOTする，Pcは複数条件振る
+    for AR in ar_array3:
+        k = 0
+        for OF in of_array3:
+            data = cea_fac(OF = OF, Pc = 5.0, AR = AR, CR = 4) # 昔の人が見たら発狂しそうな記法 CRとO/Fは適当
+            print data
+            vsof_data_array[k] = np.array(data)
+            k+=1
+        g = 9.80665
+        Isp = vsof_data_array[:,2] / g # 海面高度比推力 sec
+        # ---- PLOT ----
+        plt.plot(of_array3, Isp, label='AR=%1.f' % (AR))
+        save_array = np.vstack([save_array, vsof_data_array])
+
+    # ---- CSVで結果を保存 ----
+    header = u'Pc[MPa], O/F[-], Isp_sea[sec], Isp_vac[sec], AR[-], cf[-]'
+    np.savetxt(vsof_data_file, save_array, fmt='%.3f', delimiter=',', header = header)
+
+    # ---- PLOTの設定 ----
+    plt.xlim(1, 3)
+    plt.xlabel('O/F')
+    plt.ylabel('Isp (sec)')
+    plt.grid()
+    plt.title('LOX/%s 100% frozen nfz=3' % (fuel))
+    plt.legend(loc='best', fontsize=12)
+    plt.savefig('ceatest_IspvsOF.png')
+    plt.close()
+
+
+
+    # ---- パラメータ指定 ----
+    ar_array4 = np.linspace(1.01, 8, 8) # 適っ当にPc=1, 3, 5, 7MPa
+    pa_array4 = np.linspace(0, 0.101, 102) # ARは適っ当に2から10までを50分割してみる
+    vspa_data_array = np.zeros((len(pa_array4), 6))
+    save_array = np.zeros([1,6]) # CSVに書き込むデータのストック場所
+    # ---- コマンドをループさせる ----
+    # この例では開口比ARを横軸、Ispを縦軸にとってPLOTする，Pcは複数条件振る
+    for AR in ar_array4:
+        k = 0
+        for pa in pa_array4:
+            data = cea_fac(OF = 1.48, Pc = 1., AR = AR, CR = 4, pa = pa) # 昔の人が見たら発狂しそうな記法 CRとO/Fは適当
+            print data
+            vspa_data_array[k] = np.array(data)
+            k+=1
+        g = 9.80665
+        Isp = vspa_data_array[:,2] / g # 海面高度比推力 sec
+        cf =  vspa_data_array[:,5]
+        # ---- PLOT ----
+        plt.plot(pa_array4, cf, label='AR=%.1f' % (AR))
+        save_array = np.vstack([save_array, vspa_data_array])
+
+    # ---- CSVで結果を保存 ----
+    header = u'Pc[MPa], O/F[-], Isp_sea[sec], Isp_vac[sec], AR[-], cf[-]'
+    np.savetxt(vspa_data_file, save_array, fmt='%.3f', delimiter=',', header = header)
+
+    # ---- PLOTの設定 ----
+#    plt.xlim(1, 10)
+    plt.xlabel('pa')
+    plt.ylabel('cf (-)')
+    plt.grid()
+    plt.title('LOX/%s 100% frozen nfz=3' % (fuel))
+    plt.legend(loc='best', fontsize=12)
+    plt.savefig('ceatest_Ispvspa.png')
+    plt.close()
+
